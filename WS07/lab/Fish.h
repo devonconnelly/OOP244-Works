0534d8154e5540b14259fe30c7e0efd59e6b5122
Devon <devonconnelly11@gmail.com>
2023-03-14 18:39:00 -0400

    finished WS07 with citation

--- WS07/lab/Fish.h
+++ WS07/lab/Fish.h
@@ -1,3 +1,8 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// Name: Devon Connelly
+// Email: dconnelly@myseneca.ca
+// ID: 105322218
+// Date Completed: Mar 14 2023
 #ifndef SDDS_FISH_H
 #define SDDS_FISH_H
 #include "Pet.h"
