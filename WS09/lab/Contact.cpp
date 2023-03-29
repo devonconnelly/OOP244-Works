ce2562160f84be41489ce0a207eed0efe0149e02
Devon <devonconnelly11@gmail.com>
2023-03-29 14:10:42 -0400

    WS09 Complete

--- WS09/lab/Contact.cpp
+++ WS09/lab/Contact.cpp
@@ -1,13 +1,17 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// Name: Devon Connelly
+// Email: dconnelly@myseneca.ca
+// ID: 105322218
+// Date Completed: Mar 29 2023
 #include <iostream>
 #include "Person.h"
 #include "Contact.h"
 #include "Tools.h"
 namespace sdds
 {
-Contact::Contact() { 
-}
+Contact::Contact() {}
 
-Contact::Contact(const sdds::Contact &copyFrom) { 
+Contact::Contact(const Contact &copyFrom) : Person(copyFrom) {
     operator=(copyFrom);
 }
 
@@ -30,16 +34,11 @@ Contact::~Contact() {
 std::istream& Contact::read(std::istream& istr) {
     ~*this;
     Person::read(istr);
-        m_address = dynRead(istr, ',');
+    m_address = dynRead(istr, ',');
     m_city = dynRead(istr, ',');
-    char* buffer = dynRead(istr, ',');
-    strCpy(m_province, buffer);
-    delete[] buffer;
-    buffer = nullptr;
-    buffer = dynRead(istr, '\n');
-    strCpy(m_postalCode, buffer);
-    delete[] buffer;
-    if(istr.fail()) ~*this; //
+    istr.getline(m_province, 3,  ',');
+    istr.getline(m_postalCode, 7,  '\n');
+    if(istr.fail()) ~*this;
     return istr;
 }
 
@@ -48,7 +47,16 @@ std::ostream& Contact::write(std::ostream& ostr) const {
     if(*this) {
         Person::write(ostr);
         ostr << std::endl;
-        ostr << m_address << std::endl << m_city << std::endl << m_province << std::endl << m_postalCode;
+        ostr << m_address << std::endl << m_city << " " << m_province << std::endl;
+        for(int i = 0; i < strLen(m_postalCode); i++)
+        {
+            if(i == 3)
+            {
+                ostr << " ";
+            }
+            ostr << m_postalCode[i];
+        }
+        ostr << std::endl;
     }
     return ostr;
 }
@@ -57,11 +65,12 @@ Contact::operator bool() const {
     return Person::operator bool() && m_address[0] && m_city[0] && m_province[0] && m_postalCode[0];
 }
 
-void Contact::operator~() { 
+void Contact::operator~() {
     Person::operator~();
     delete[] m_address;
     delete[] m_city;
     m_address = m_city = nullptr;
+    m_province[0] = m_postalCode[0] = '\0';
 }
 
 }
--- WS09/lab/Contact.h
+++ WS09/lab/Contact.h
@@ -1,3 +1,8 @@
+// I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+// Name: Devon Connelly
+// Email: dconnelly@myseneca.ca
+// ID: 105322218
+// Date Completed: Mar 29 2023
 #ifndef SDDS_CONTACT_H
 #define SDDS_CONTACT_H
 #include "Person.h"
@@ -7,8 +12,8 @@ class Contact: public Person {
 private:
     char* m_address = nullptr;
     char* m_city = nullptr;
-    char m_province[2]{};
-    char m_postalCode[6]{};
+    char m_province[3]{};
+    char m_postalCode[7]{};
 public:
     Contact();
     Contact(const Contact& copyFrom);
